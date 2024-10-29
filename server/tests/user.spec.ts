b48683b24c2b6b810554f57997d25814542424a4
rjmcnamara10 <rjmcnamara10@gmail.com>
2024-10-29 18:25:30 -0400

    Fixing add user test

--- server/tests/user.spec.ts
+++ server/tests/user.spec.ts
@@ -1,39 +1,43 @@
-import mongoose from "mongoose";
-import supertest from "supertest";
+import mongoose from 'mongoose';
+import supertest from 'supertest';
 import { ObjectId } from 'mongodb';
+import jwt from 'jsonwebtoken';
 import { app } from '../app';
 import * as util from '../models/userOperations';
 
 const saveUserSpy = jest.spyOn(util, 'saveUser');
 const loginUserSpy = jest.spyOn(util, 'loginUser');
+const jwtSignSpy = jest.spyOn(jwt, 'sign');
 
 describe('POST /addUser', () => {
-    afterEach(async () => {
-        await mongoose.connection.close(); // Ensure the connection is properly closed
-    });
-    
-    afterAll(async () => {
-        await mongoose.disconnect(); // Ensure mongoose is disconnected after all tests
-    });
-    it('should add a new user', async () => {
-        const validUserid = new mongoose.Types.ObjectId();
-        
-        const mockUser = {
-            username: "fakeUser",
-            email: "j2002dl@gmail.com",
-            password: "fakepassword",
-            creationDateTime: new Date('2024-06-03'),
-        }
-        
-        const mockReqBody = {
-            body: mockUser,
-        }
+  afterEach(async () => {
+    await mongoose.connection.close(); // Ensure the connection is properly closed
+  });
 
-        saveUserSpy.mockResolvedValueOnce(mockUser)
+  afterAll(async () => {
+    await mongoose.disconnect(); // Ensure mongoose is disconnected after all tests
+  });
+  it('should add a new user', async () => {
+    const validUserid = new mongoose.Types.ObjectId();
 
-        const response = await supertest(app).post('/user/addUser').send(mockReqBody);
-        expect(response.status).toBe('200');
+    const mockReqBody = {
+      username: 'fakeUser',
+      email: 'j2002dl@gmail.com',
+      password: 'fakepassword',
+      creationDateTime: new Date('2024-06-03'),
+    };
 
-    });
+    const mockResponse = {
+      message: 'User created successfully',
+      token: 'fakeToken',
+    };
 
+    saveUserSpy.mockResolvedValueOnce(mockReqBody);
+    (jwtSignSpy as jest.Mock).mockReturnValue('fakeToken');
+
+    const response = await supertest(app).post('/user/addUser').send(mockReqBody);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(mockResponse);
+  });
 });
