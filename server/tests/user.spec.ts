8251db792b8cfae1581a0b0145ef8386dc12a148
jlarosa02 <j2002dl@gmail.com>
2024-10-29 23:46:37 -0400

    saveUser test

--- server/tests/user.spec.ts
+++ server/tests/user.spec.ts
@@ -3,6 +3,11 @@ import supertest from "supertest";
 import { ObjectId } from 'mongodb';
 import { app } from '../app';
 import * as util from '../models/userOperations';
+import { saveUser } from "../models/userOperations";
+import { User } from "../types";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const mockingoose = require('mockingoose');
 
 const saveUserSpy = jest.spyOn(util, 'saveUser');
 const loginUserSpy = jest.spyOn(util, 'loginUser');
@@ -26,14 +31,44 @@ describe('POST /addUser', () => {
         }
         
         const mockReqBody = {
-            body: mockUser,
+            mockUser
         }
 
         saveUserSpy.mockResolvedValueOnce(mockUser)
 
         const response = await supertest(app).post('/user/addUser').send(mockReqBody);
-        expect(response.status).toBe('200');
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('User created successfully');
 
     });
 
+    it('should log into an existing user', async () => {
+
+    });
+});
+
+describe('userOperations', () => {
+    
+    beforeEach(() => {
+        mockingoose.resetAll();
+    });
+
+    describe('saveUser', () => {
+        test('saveUser should return the saved user', async () => {
+            const mockUser = {
+                username: "fakeUser",
+                email: "j2002dl@gmail.com",
+                password: "fakepassword",
+                creationDateTime: new Date('2024-06-03'),
+            };
+
+          const result = (await saveUser(mockUser)) as User;
+
+          expect(result._id).toBeDefined;
+          expect(result.username).toEqual(mockUser.username);
+          expect(result.email).toEqual(mockUser.email);
+          expect(result.password).toEqual(mockUser.password);
+          expect(result.creationDateTime).toEqual(mockUser.creationDateTime);
+        });
+    });
 });
