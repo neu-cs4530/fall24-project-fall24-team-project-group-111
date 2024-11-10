fe3655a2bf8f9ffe8cf85c37d84c9a5b743d5b89
lubainamalvi <malvi.l@northeastern.edu>
2024-11-10 14:10:45 -0500

    removing unused import OpenAI

--- server/controller/answer.ts
+++ server/controller/answer.ts
@@ -1,5 +1,4 @@
 import express, { Response } from 'express';
-import OpenAI from 'openai';
 import { Answer, AnswerRequest, AnswerResponse, FakeSOSocket } from '../types';
 import { addAnswerToQuestion, populateDocument, saveAnswer } from '../models/application';
 
