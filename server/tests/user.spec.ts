43d7b33f5cf0cf92fabed2a758fad7529ddabc01
lubainamalvi <malvi.l@northeastern.edu>
2024-11-20 19:38:48 -0500

    backend testing

--- server/tests/user.spec.ts
+++ server/tests/user.spec.ts
@@ -11,6 +11,10 @@ const loginUserSpy = jest.spyOn(util, 'loginUser');
 const sendPasswordResetSpy = jest.spyOn(util, 'sendPasswordReset');
 const resetPasswordSpy = jest.spyOn(util, 'resetPassword');
 const jwtSignSpy = jest.spyOn(jwt, 'sign');
+const changeThemeSpy = jest.spyOn(util, 'changeTheme');
+const changeFontSpy = jest.spyOn(util, 'changeFont');
+const changeTextSizeSpy = jest.spyOn(util, 'changeTextSize');
+const changeTextBoldnessSpy = jest.spyOn(util, 'changeTextBoldness');
 
 const mockSettingsInfo = {
   theme: 'LightMode',
@@ -163,6 +167,19 @@ describe('POST /emailVerification', () => {
       'Error when sending email verification: Error sending email verification',
     );
   });
+  it('should return a generic 500 error if an unknown error occurs', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      email: 'fakeEmail@email.com',
+      password: 'fakepassword',
+      creationDateTime: new Date('2024-06-03'),
+    };
+
+    sendEmailVerificationSpy.mockRejectedValue('Unexpected failure');
+    const response = await supertest(app).post('/user/emailVerification').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when sending email verification');
+  });
 });
 
 describe('POST /addUser', () => {
@@ -235,6 +252,12 @@ describe('POST /addUser', () => {
     const response = await supertest(app).post('/user/addUser').send({ token: 'fakeToken' });
     expect(response.status).toBe(500);
     expect(response.text).toBe('Error when saving user: Error signing token');
+  });
+  it('should return a generic 500 error if an unknown error occurs', async () => {
+    saveUserSpy.mockRejectedValueOnce('Unexpected error');
+    const response = await supertest(app).post('/user/addUser').send({ token: 'fakeToken' });
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when saving user');
   });
 });
 
@@ -369,6 +392,17 @@ describe('POST /loginUser', () => {
     expect(response.status).toBe(500);
     expect(response.text).toBe('Error when logging in: Error signing token');
   });
+  it('should return a 500 error with a generic message if an unknown error occurs in the catch block', async () => {
+    loginUserSpy.mockRejectedValueOnce({});
+    const mockReqBody = {
+      username: 'fakeUser',
+      password: 'fakePassword',
+    };
+
+    const response = await supertest(app).post('/user/loginUser').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when logging in');
+  });
 });
 
 describe('POST /sendPasswordReset', () => {
@@ -433,6 +467,15 @@ describe('POST /sendPasswordReset', () => {
     expect(response.text).toBe(
       'Error when sending password reset: Error sending password reset email',
     );
+  });
+  it('should return a 500 error with a generic message if an unknown error occurs in the catch block', async () => {
+    sendPasswordResetSpy.mockRejectedValueOnce({});
+    const mockReqBody = {
+      username: 'fakeUser',
+    };
+    const response = await supertest(app).post('/user/sendPasswordReset').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when sending password reset');
   });
 });
 
@@ -539,5 +582,293 @@ describe('POST /resetPassword', () => {
 
     expect(response.status).toBe(500);
     expect(response.text).toBe('Error when resetting password: Error resetting password');
+  });
+  it('should return a 500 error with a generic message if an unknown error occurs in the catch block', async () => {
+    resetPasswordSpy.mockRejectedValueOnce({});
+    const mockReqBody = {
+      token: 'fakeToken',
+      newPassword: 'newPassword',
+    };
+    const response = await supertest(app).post('/user/resetPassword').send(mockReqBody);
+
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when resetting password');
+  });
+});
+describe('POST /changeTheme', () => {
+  afterEach(async () => {
+    await mongoose.connection.close(); // Ensure the connection is properly closed
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect(); // Ensure mongoose is disconnected after all tests
+  });
+  it('should return a 400 error if username or theme is missing', async () => {
+    // missing both username and theme
+    const response1 = await supertest(app).post('/user/changeTheme').send({});
+    expect(response1.status).toBe(400);
+    expect(response1.text).toBe('Invalid request');
+
+    // missing just username
+    const response2 = await supertest(app).post('/user/changeTheme').send({ theme: 'darkMode' });
+    expect(response2.status).toBe(400);
+    expect(response2.text).toBe('Invalid request');
+    // missing just theme
+    const response3 = await supertest(app).post('/user/changeTheme').send({ username: 'fakeUser' });
+    expect(response3.status).toBe(400);
+    expect(response3.text).toBe('Invalid request');
+  });
+
+  it('should return a success message and updated user if theme update is successful', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      theme: 'dark',
+    };
+
+    const mockResponse = {
+      username: 'fakeUser',
+      theme: 'dark',
+      email: 'fakeEmail@email.com',
+      password: 'password',
+      creationDateTime: new Date(),
+    };
+
+    changeThemeSpy.mockResolvedValueOnce(mockResponse);
+    const response = await supertest(app).post('/user/changeTheme').send(mockReqBody);
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Theme update successful');
+    expect(response.body.user.theme).toBe('dark');
+  });
+  it('should return a 500 error with the correct message if changeTheme throws an error', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      theme: 'dark',
+    };
+    changeThemeSpy.mockResolvedValueOnce({ error: 'User not found' });
+    const response = await supertest(app).post('/user/changeTheme').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating theme: User not found');
+  });
+
+  it('should return a 500 error with a generic message if an unknown error occurs in the catch block', async () => {
+    changeThemeSpy.mockRejectedValueOnce({});
+    const mockReqBody = {
+      username: 'fakeUser',
+      theme: 'dark',
+    };
+    const response = await supertest(app).post('/user/changeTheme').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating theme');
+  });
+});
+
+describe('POST /changeFont', () => {
+  afterEach(async () => {
+    await mongoose.connection.close(); // Ensure the connection is properly closed
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect(); // Ensure mongoose is disconnected after all tests
+  });
+  it('should successfully update the font', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      font: 'Arial',
+    };
+
+    const mockResponse = {
+      username: 'fakeUser',
+      email: 'fakeEmail@email.com',
+      font: 'Arial',
+      password: 'password',
+      creationDateTime: new Date(),
+    };
+    changeFontSpy.mockResolvedValueOnce(mockResponse);
+    const response = await supertest(app).post('/user/changeFont').send(mockReqBody);
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Font update successful');
+    expect(response.body.user.font).toBe('Arial');
+  });
+  it('should return a 400 error if the request body is missing required fields', async () => {
+    const response = await supertest(app).post('/user/changeFont');
+    expect(response.status).toBe(400);
+    expect(response.text).toBe('Invalid request');
+  });
+  it('should return a 400 error if the username field is missing', async () => {
+    const mockReqBody = {
+      font: 'Arial',
+    };
+
+    const response = await supertest(app).post('/user/changeFont').send(mockReqBody);
+    expect(response.status).toBe(400);
+    expect(response.text).toBe('Invalid request');
+  });
+  it('should return a 400 error if the font field is missing', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+    };
+
+    const response = await supertest(app).post('/user/changeFont').send(mockReqBody);
+    expect(response.status).toBe(400);
+    expect(response.text).toBe('Invalid request');
+  });
+  it('should return a 500 error if an unknown error occurs in the changeFont function', async () => {
+    changeFontSpy.mockRejectedValueOnce(new Error('Database connection failed'));
+    const mockReqBody = {
+      username: 'fakeUser',
+      font: 'Arial',
+    };
+
+    const response = await supertest(app).post('/user/changeFont').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating font: Database connection failed');
+  });
+  it('should return a 500 error with a generic message if an unknown error occurs', async () => {
+    changeFontSpy.mockRejectedValueOnce({});
+
+    const mockReqBody = {
+      username: 'fakeUser',
+      font: 'Arial',
+    };
+    const response = await supertest(app).post('/user/changeFont').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating font');
+  });
+  it('should throw an error if userFromDb contains an error property', async () => {
+    const mockErrorResponse = {
+      error: 'Font change failed due to some issue',
+    };
+
+    changeFontSpy.mockResolvedValueOnce(mockErrorResponse);
+    const mockReqBody = {
+      username: 'fakeUser',
+      font: 'Arial',
+    };
+    const response = await supertest(app).post('/user/changeFont').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating font: Font change failed due to some issue');
+  });
+});
+
+describe('POST /changeTextSize', () => {
+  afterEach(async () => {
+    await mongoose.connection.close(); // Ensure the connection is properly closed
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect(); // Ensure mongoose is disconnected after all tests
+  });
+  it('should return a 400 error when the request body is missing username or textSize', async () => {
+    const mockReqBody1 = {
+      textSize: '16px',
+    };
+    const response1 = await supertest(app).post('/user/changeTextSize').send(mockReqBody1);
+    expect(response1.status).toBe(400);
+    expect(response1.text).toBe('Invalid request');
+    const mockReqBody2 = {
+      username: 'fakeUser',
+    };
+    const response2 = await supertest(app).post('/user/changeTextSize').send(mockReqBody2);
+    expect(response2.status).toBe(400);
+    expect(response2.text).toBe('Invalid request');
+  });
+
+  it('should proceed when both username and textSize are provided', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      textSize: '16px',
+    };
+    const mockResponse = {
+      username: 'fakeUser',
+      email: 'fakeEmail@email.com',
+      font: 'Arial',
+      password: 'password',
+      creationDateTime: new Date(),
+    };
+    changeTextSizeSpy.mockResolvedValueOnce(mockResponse);
+    const response = await supertest(app).post('/user/changeTextSize').send(mockReqBody);
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Text size update successful');
+  });
+
+  it('should throw an error when userFromDb contains an error', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      textSize: '16px',
+    };
+    const mockErrorResponse = { error: 'Invalid text size' };
+    changeTextSizeSpy.mockResolvedValueOnce(mockErrorResponse);
+
+    const response = await supertest(app).post('/user/changeTextSize').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating text size: Invalid text size');
+  });
+  it('should return a 500 error with the error message if err is an instance of Error', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      textSize: '16px',
+    };
+    const mockError = new Error('Some unexpected error');
+    changeTextSizeSpy.mockRejectedValueOnce(mockError);
+
+    const response = await supertest(app).post('/user/changeTextSize').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating text size: Some unexpected error');
+  });
+  it('should return a 500 error with a generic message if err is not an instance of Error', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      textSize: '16px',
+    };
+    const mockNonError = {};
+    changeTextSizeSpy.mockRejectedValueOnce(mockNonError);
+
+    const response = await supertest(app).post('/user/changeTextSize').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating text size');
+  });
+});
+describe('POST /changeTextBoldness', () => {
+  afterEach(async () => {
+    await mongoose.connection.close(); // Ensure the connection is properly closed
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect(); // Ensure mongoose is disconnected after all tests
+  });
+  it('should return a 400 error if username or textBoldness is missing', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+    };
+
+    const response = await supertest(app).post('/user/changeTextBoldness').send(mockReqBody);
+    expect(response.status).toBe(400);
+    expect(response.text).toBe('Invalid request');
+  });
+
+  it('should return a 500 error when the response includes an error field', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      textBoldness: 'bold',
+    };
+    const mockErrorResponse = {
+      error: 'Database update failed',
+    };
+
+    changeTextBoldnessSpy.mockResolvedValueOnce(mockErrorResponse);
+    const response = await supertest(app).post('/user/changeTextBoldness').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating text boldness: Database update failed');
+  });
+  it('should return a 500 error with a generic message if an unknown error occurs', async () => {
+    const mockReqBody = {
+      username: 'fakeUser',
+      textBoldness: 'bold',
+    };
+    changeTextBoldnessSpy.mockRejectedValueOnce({});
+
+    const response = await supertest(app).post('/user/changeTextBoldness').send(mockReqBody);
+    expect(response.status).toBe(500);
+    expect(response.text).toBe('Error when updating text boldness');
   });
 });
